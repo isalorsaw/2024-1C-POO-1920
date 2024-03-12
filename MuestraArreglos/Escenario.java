75e39b8596f1d1a2eb27a280672042a5987a2a06
isalorsaw <isalorsaw@hotmail.com>
2024-03-11 20:05:10 -0600

    Mov en Automatico con Timer 11/03

--- MuestraArreglos/Escenario.java
+++ MuestraArreglos/Escenario.java
@@ -1,19 +1,38 @@
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.JOptionPane;
 import java.awt.Graphics;
 import java.awt.event.KeyListener;//Libreria para evento de teclado
 import java.awt.event.KeyEvent;//Libreria para utilizar variable de evento del teclado
-public class Escenario extends JPanel implements KeyListener
+import java.awt.event.ActionListener;//Libreria para eventos de click o timer
+import java.awt.event.ActionEvent;//Libreria para utilizar variabla de evento de click o timer
+public class Escenario extends JPanel implements KeyListener,ActionListener
 {
     Fondo f;
     //Yate y;
     Yate y[]=new Yate[10];
+    Yate v;
+    Timer t;
+    int aleatorio;
+    int segundos;
     public Escenario()
     {
         f=new Fondo("imagenes/fondo2.png",0,0);
         //y=new Yate("imagenes/lancha.png",10,450);
         inicializarYates();//Inicializar el arreglo de Yates de forma aleatoria
         imprimirYates();
+        v=new Yate("imagenes/velero.png",100,380);
         //System.out.println(f.toString());
+       
+        //Instanciar el Timer
+        aleatorio=ClaseFM.generaAleatorio(1,2);
+        JOptionPane.showMessageDialog(null,aleatorio+"");
+        segundos=0;
+        t=new Timer(50,null);//Cada 5 milisegundos se va a ejectuar el actionPerformed
+        t.start();
+        t.addActionListener(this);//Agregando en el JPanel el evento de accion del click o timer
+        
+        
         addKeyListener(this);//Agregar en en JPanel el evento del teclado
         setFocusable(true);//Linea para priorizar el Panel
         setVisible(true);
@@ -40,6 +59,36 @@ public class Escenario extends JPanel implements KeyListener
         {
             y[i].dibuja(g);
         }
+    }
+    public void actionPerformed(ActionEvent evt)
+    {
+        if(evt.getSource()==t)
+        {
+            //System.out.println(segundos+"");
+            segundos++;
+            
+            char direccion=' ';
+            String rutay="";
+            if(aleatorio==1)
+            {
+                direccion='l';//Izquierda
+                rutay="imagenes/lancha_izq.png";
+            }
+            else if(aleatorio==2)
+            {
+                direccion='r';//Derecha
+                rutay="imagenes/lancha_der.png";
+            }
+            
+            
+            v.mover(direccion,"imagenes/velero.png");
+            
+            //Mover todos los yates a la derecha
+            moverYates(direccion,rutay);
+            
+            repaint();
+        }
+        //if(evt.getSource()==t2)
     }
     public void keyTyped(KeyEvent evt)
     {
@@ -82,6 +131,7 @@ public class Escenario extends JPanel implements KeyListener
         super.paint(g);
         g.drawString("Elaborado por Ing Isaias Salinas",200,200);
         f.dibuja(g);
+        v.dibuja(g);
         //y.dibuja(g);
         dibujarYates(g);//Metodo para dibujar el arreglo de yates
     }
